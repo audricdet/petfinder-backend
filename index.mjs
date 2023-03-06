c7c14dba536d979e85f40ee96d9bdbad217f5756
audricdet <audric.det@hotmail.com>
2023-03-06 16:31:06 +0100

    First try to insertProfileInfos

--- index.mjs
+++ index.mjs
@@ -30,6 +30,11 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser())
 app.use(express.json())
+app.use(session({
+    secret: 'coucou',
+    resave: false,
+    saveUninitialized: false
+}));
 
 //CORS
 app.use(cors())
