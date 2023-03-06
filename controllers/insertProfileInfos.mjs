c8bc0b6b1459b0bb689c0270e0113e72ff636df3
audricdet <audric.det@hotmail.com>
2023-03-06 16:50:56 +0100

    First try to insertProfileInfos

--- controllers/insertProfileInfos.mjs
+++ controllers/insertProfileInfos.mjs
@@ -1,34 +1,40 @@
 import client from "../src/db/connect.mjs";
-import session from "express-session";
 
 const insertProfileInfos = async (request, response) => {
+    console.log(request.body)
     const {
         name,
         lastname,
         email,
         phone,
-        adress,
+        adress, 
         user_id
     } = request.body;
 
-    if (userId) {
-        await client.query(
-            `INSERT INTO profile (name, lastname, email, phone, adress, user_id)
-            VALUES ($1, $2, $3, $4, $5, $6)`,
-            [name, lastname, email, phone, adress, user_id],
-            (error, result) => {
+await client.query(
+        `INSERT INTO profile (name,
+            lastname,
+            email,
+            phone,
+            adress, 
+            user_id)
+        VALUES ($1, $2, $3, $4, $5, $6)`,
+        [name,
+            lastname,
+            email,
+            phone,
+            adress, 
+            user_id],
+        (error, result) => {
             if (error) {
                 throw error;
-            } else {
-                console.log(result);
-                response.send({ result: result });
+            }else{
+                console.log(result)
+                response.send({result: result})
             }
-            }
-        );
-    } else {
-        // Handle the case where userId is null
-        response.status(400).send({ error: 'No user ID provided' });
-    }
+            
+        }
+    );
 };
 
 export default insertProfileInfos;
