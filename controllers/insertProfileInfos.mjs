9ccf24a14ebf48353b1ddd40a0e9b666a7f86762
audricdet <audric.det@hotmail.com>
2023-03-06 16:48:17 +0100

    First try to insertProfileInfos

--- controllers/insertProfileInfos.mjs
+++ controllers/insertProfileInfos.mjs
@@ -2,21 +2,20 @@ import client from "../src/db/connect.mjs";
 import session from "express-session";
 
 const insertProfileInfos = async (request, response) => {
-    const userId = request.session.userId; // Récupère l'ID de l'utilisateur connecté depuis la session
-    console.log(userId)
     const {
         name,
         lastname,
         email,
         phone,
-        adress
+        adress,
+        user_id
     } = request.body;
 
     if (userId) {
         await client.query(
             `INSERT INTO profile (name, lastname, email, phone, adress, user_id)
             VALUES ($1, $2, $3, $4, $5, $6)`,
-            [name, lastname, email, phone, adress, userId],
+            [name, lastname, email, phone, adress, user_id],
             (error, result) => {
             if (error) {
                 throw error;
